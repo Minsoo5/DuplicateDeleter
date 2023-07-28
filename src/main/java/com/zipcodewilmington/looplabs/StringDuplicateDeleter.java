f91428dce0e5adc3aa11cb0b235fa169f7a9a4cd
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-07-28 08:14:44 -0400

    String Duplicate Deleter Done

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -11,11 +11,54 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String holder = "";
+        int howManyToRemove = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    counter++;
+                }
+                if (counter >= maxNumberOfDuplications) {
+                    howManyToRemove++;
+                }
+            }
+            if (counter < maxNumberOfDuplications) {
+                holder += array[i] + " ";
+            }
+        }
+        if (holder.length() < 1) {
+            String[] emptyString = new String[0];
+            return emptyString;
+        }
+        String[] stringArr = holder.split(" ");
+
+        return stringArr;
     }
+
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String holder = "";
+        int howManyToRemove = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
+                    counter++;
+                }
+                if (counter == exactNumberOfDuplications) {
+                    howManyToRemove++;
+                }
+            }
+            if (counter != exactNumberOfDuplications) {
+                holder += array[i] + " ";
+            }
+        }
+        String[] stringArr = holder.split(" ");
+
+        return stringArr;
     }
 }
